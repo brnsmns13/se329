e9797bd9b21b5674c5978bcd032a6619d2b0a930
Brian Simons <brnsmns13@gmail.com>
2015-02-19 13:01:52 -0600

    Added basic template

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
+import os
+
+from google.appengine.api import users
+from google.appengine.ext import ndb
+
+import jinja2
 import webapp2
+
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(
+        os.path.join(os.path.dirname(__file__), 'templates')),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        self.response.write('<h1>Hello world</h1>');
+        template = JINJA_ENVIRONMENT.get_template('index.html')
+        self.response.write(template.render());
 
 
 application = webapp2.WSGIApplication([
