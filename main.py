49e8298cadbe34110beff35a52c6bdedeb2afb7b
Brian Simons <brnsmns13@gmail.com>
2015-02-23 22:28:08 -0600

    Created base response page

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from google.appengine.ext import ndb
 import jinja2
 import webapp2
 
+ANSWERS = ['A', 'B', 'C', 'D']
+USER = 'test_user@iastate.edu'
+
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(
@@ -20,6 +23,28 @@ class MainPage(webapp2.RequestHandler):
         self.response.write(template.render());
 
 
+class ResponsePage(webapp2.RequestHandler):
+    def get(self):
+        template = JINJA_ENVIRONMENT.get_template('respond.html')
+        template_values = {
+            'username': USER,
+            'pin_default': 0
+        }
+        self.response.write(template.render(template_values));
+
+    def post(self):
+        pin = int(self.request.get('pin'))
+        current_answer = int(self.request.get('answer'))
+        template_values = {
+            'username': USER,
+            'pin_default': pin,
+            'current_answer': ANSWERS[current_answer]
+        }
+        template = JINJA_ENVIRONMENT.get_template('respond.html')
+        self.response.write(template.render(template_values));
+
+
 application = webapp2.WSGIApplication([
-    ('/', MainPage)
+    ('/', MainPage),
+    ('/respond', ResponsePage)
 ], debug=True)
