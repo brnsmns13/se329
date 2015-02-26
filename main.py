85c1f0fda97f2224e769132d288c19dfa7468b19
Brian Simons <brnsmns13@gmail.com>
2015-02-26 10:49:48 -0600

    Added create quiz form

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
+import jinja2
 import json
+import logging
 import os
+import random
+import webapp2
 
 from google.appengine.api import users
 from google.appengine.ext import ndb
 
-import jinja2
-import webapp2
+from models import *
 
-ANSWERS = ['A', 'B', 'C', 'D']
+
+ANSWERS = [None, 'A', 'B', 'C', 'D']
 USER = 'test_user@iastate.edu'
+
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(
@@ -16,6 +21,7 @@ JINJA_ENVIRONMENT = jinja2.Environment(
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 	
+
 class MainPage(webapp2.RequestHandler):
     def get(self):
         template = JINJA_ENVIRONMENT.get_template('index.html')
@@ -49,39 +55,40 @@ class CreatePage(webapp2.RequestHandler):
         template = JINJA_ENVIRONMENT.get_template('create.html')
         self.response.write(template.render())
 		
-	def post(self):
-		data = json.loads(self.request.body)
-		
-		quiz = Quiz()
-		for question in data['questionArr']
-				question = Question(content=question['question'],
-									answers={question['a'],question['b'],question['c'],question['d'],
-									correct_answer=question['correctAnswer'],
-									name=question['name'])
-				quiz.questions.append(question)
-		quiz.put()
+    def post(self):
+        logging.info(self.request.body)
+        data = json.loads(self.request.body)
+        quiz = Quiz()
+        for question in data['questionArr']:
+            question = Question(content=question['question'],
+                answers=[question['a'],question['b'],question['c'],question['d']],
+                correct_answer=question['correctAnswer'])
+            quiz.questions.append(question)
+        quiz.name = "Quiz 1"
+        quiz.put()
 				
 class QuizPage(webapp2.RequestHandler):
-	def get(self):
-		quizzes = Quiz.query().fetch(10)
-		
-		template_values = {
-			'quizzes' : quizzes
-		}
-		
-		template = JINJA_ENVIRONMENT.get_template('quizzes.html')
+    def get(self):
+        quizzes = Quiz.query().fetch(10)
+        template_values = {
+            'quizzes' : quizzes
+        }
+        
+        template = JINJA_ENVIRONMENT.get_template('quizzes.html')
         self.response.write(template.render(template_values));
 	
 class StartPage(webapp2.RequestHandler):
-	def get(self):
-		quiz = self.request.get('key')
-		rand_code = randrange(1000,10000)
-		quiz.code=rand_code
-		template_values = {
-			'quizzes' : quizzes
-		}
-		
-		template = JINJA_ENVIRONMENT.get_template('start.html')
+    def get(self):
+        quiz_key = self.request.get('key')
+        quiz = Quiz.get_by_id(quiz_key)
+        rand_code = random.randint(1000,2000)
+        quiz.code=rand_code
+        quiz.put()
+        template_values = {
+            'quizzes' : quizzes
+        }
+        
+        template = JINJA_ENVIRONMENT.get_template('start.html')
         self.response.write(template.render(template_values));
 		
 			
@@ -89,6 +96,6 @@ application = webapp2.WSGIApplication([
     ('/', MainPage),
     ('/respond', ResponsePage),
     ('/create', CreatePage),
-	('/quizzes', QuizPage),
-	('/start', StartPage)
+    ('/quizzes', QuizPage),
+    ('/start', StartPage)
 ], debug=True)
---
+++ static/js/quiz.js
@@ -0,0 +1,42 @@
+
+var questionArr = [];
+
+function quizQuestion(q,a,b,c,d,ans){
+	var question = {};
+	question.question = q;
+	question.a = a;
+	question.b = b;
+	question.c = c;
+	question.d = d;
+	question.correctAnswer = ans;
+	return question;
+}
+
+function getFormData(){
+	var question = document.getElementById("element_1").value;
+	var a = document.getElementById("element_2").value;
+	var b = document.getElementById("element_3").value;
+	var c = document.getElementById("element_4").value;
+	var d = document.getElementById("element_5").value;
+	var correct = 'a';
+	
+	var newQuestion = quizQuestion(question, a,b,c,d, correct);
+	questionArr.push(newQuestion);
+	console.log(newQuestion);
+	console.log(questionArr);
+	return false;
+}
+
+function clearArr(){
+	questionArr = [];
+	alert("cleared");
+}
+
+function toJSON(){
+	var data = {
+		"questionArr" : questionArr
+	};
+	
+	$.post("/create", JSON.stringify(data));
+    window.location.replace("/quizzes");
+}
