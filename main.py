70c035455ca3eabcc0f9238b5a9c19a2ed3e803e
CivBase <blange@iastate.edu>
2015-03-09 18:09:08 -0500

    Cleaned some stuff up
    - Added README and COMMANDS documentation
    - Cleaned up python files to support pep8
    - Cleaned up html files to be a bit more consistent
    - Gave the app an actual name (albeit a horrible one)
    - Added more to the gitignore

--- main.py
+++ main.py
@@ -20,12 +20,12 @@ JINJA_ENVIRONMENT = jinja2.Environment(
         os.path.join(os.path.dirname(__file__), 'templates')),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
-	
+
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
         template = JINJA_ENVIRONMENT.get_template('index.html')
-        self.response.write(template.render());
+        self.response.write(template.render())
 
 
 class ResponsePage(webapp2.RequestHandler):
@@ -35,7 +35,7 @@ class ResponsePage(webapp2.RequestHandler):
             'username': USER,
             'pin_default': 0
         }
-        self.response.write(template.render(template_values));
+        self.response.write(template.render(template_values))
 
     def post(self):
         pin = int(self.request.get('pin', -1))
@@ -47,51 +47,56 @@ class ResponsePage(webapp2.RequestHandler):
             'current_answer': ANSWERS[current_answer]
         }
         template = JINJA_ENVIRONMENT.get_template('respond.html')
-        self.response.write(template.render(template_values));
+        self.response.write(template.render(template_values))
 
 
 class CreatePage(webapp2.RequestHandler):
     def get(self):
         template = JINJA_ENVIRONMENT.get_template('create.html')
         self.response.write(template.render())
-		
+
     def post(self):
         logging.info(self.request.body)
         data = json.loads(self.request.body)
         quiz = Quiz()
         for question in data['questionArr']:
-            question = Question(content=question['question'],
-                answers=[question['a'],question['b'],question['c'],question['d']],
+            question = Question(
+                content=question['question'],
+                answers=[question['a'], question['b'],
+                         question['c'], question['d']],
                 correct_answer=question['correctAnswer'])
+
             quiz.questions.append(question)
+
         quiz.name = "Quiz 1"
         quiz.put()
-				
+
+
 class QuizPage(webapp2.RequestHandler):
     def get(self):
         quizzes = Quiz.query().fetch(10)
         template_values = {
-            'quizzes' : quizzes
+            'quizzes': quizzes
         }
         
         template = JINJA_ENVIRONMENT.get_template('quizzes.html')
-        self.response.write(template.render(template_values));
-	
+        self.response.write(template.render(template_values))
+
+
 class StartPage(webapp2.RequestHandler):
     def get(self):
         quiz_key = self.request.get('key')
         quiz = Quiz.get_by_id(quiz_key)
-        rand_code = random.randint(1000,2000)
-        quiz.code=rand_code
+        rand_code = random.randint(1000, 2000)
+        quiz.code = rand_code
         quiz.put()
         template_values = {
-            'quizzes' : quizzes
         }
         
         template = JINJA_ENVIRONMENT.get_template('start.html')
-        self.response.write(template.render(template_values));
-		
-			
+        self.response.write(template.render(template_values))
+
+
 application = webapp2.WSGIApplication([
     ('/', MainPage),
     ('/respond', ResponsePage),
